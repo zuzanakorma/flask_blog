a9096d6198ab5598f93a21bb83f8fcc4a5769a23
Zuzana Kormancova <zkormancova@Zuzanas-MBP.josegomez.io>
2020-09-19 12:43:19 +0100

    first commit

---
+++ flaskblog/config.py
@@ -0,0 +1,13 @@
+import os
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = os.environ.get('MAIL_PORT') or 587
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') or True
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+    MAIL_NO_REPLY = os.environ.get('EMAIL_NO_REPLY')
+
