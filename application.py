6e1347558009c2d2774f5e1bb2a506d0afb4afcf
Zuzana Kormancova <zunakorma@gmail.com>
2021-12-22 18:25:58 +0100

    rename app to application

--- application.py
+++ application.py
@@ -1,7 +1,8 @@
 from flaskblog import create_app
 
-app = create_app()
+application = app = create_app()
 
-if __name__=="__main__":
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    application.run()
 
